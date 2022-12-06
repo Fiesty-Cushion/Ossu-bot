e93e6b6228d3e1dc98e6e43709aebd59d38b3d22
Fiesty-Cushion <neupane.braz07@gmail.com>
2022-12-06 20:02:38 +0545

    initial commit

---
+++ src/commands/tools/daily-routine.js
@@ -0,0 +1,77 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder
+} = require("discord.js");
+
+const { Days } = require("../../../routine.json");
+
+const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+];
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("routine")
+    .setDescription("Provides Daily Routine")
+    .addStringOption((option) =>
+      option
+        .setName("day")
+        .setDescription("Routine for Provided Day")
+        .addChoices(
+          { name: "Sunday", value: "Sunday" },
+          { name: "Monday", value: "Monday" },
+          { name: "Tuesday", value: "Tuesday" },
+          { name: "Wednesday", value: "Wednesday" },
+          { name: "Thursday", value: "Thursday" },
+          { name: "Friday", value: "Friday" }
+        )
+    ),
+
+  async execute(interaction, client) {
+    var day = interaction.options.getString("day");
+
+    //in case user doesn't specify day from options, assign present day 
+    if (!day) {
+      let dayIndex = new Date().getDay();
+      const hours = new Date().getHours();
+
+      //sends next day's routine after 12PM for each day except Friday and Saturday
+      //from Friday after 12PM and all Saturday, sends Sunday's routine
+      if (hours <= 12 && dayIndex != 6) {
+        day = weekDays[dayIndex];
+      } else if (hours > 12 && (dayIndex == 6 || dayIndex == 5)) {
+        day = weekDays[0];
+      } else {
+        day = weekDays[dayIndex + 1];
+      }
+    }
+
+    //to iterate through object
+    let periods = Object.keys(Days[day]);
+    let routine = [];
+
+    //creates an array of objects containing routine info 
+    periods.forEach(function (period) {
+      let PeriodInfo = {
+        name: `${period} Period - ${Days[day][period].subject}`,
+        value: `${Days[day][period].teacher}\n${Days[day][period].time}`,
+      };
+      routine.push(PeriodInfo);
+    });
+
+    //creates the embed for routine
+    const embedRoutine = new EmbedBuilder()
+      .setTitle(`**${day}'s Routine**`)
+      .setColor(0x1abc9c)
+      .addFields(routine);
+
+    await interaction.reply({
+      embeds: [embedRoutine],
+    });
+  },
+};
---
+++ src/functions/handler/handleCommands.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const { REST, Routes } = require("discord.js");
+require('dotenv').config();
+
+module.exports = (client) => {
+  client.handleCommands = async () => {
+    const commandFolders = fs.readdirSync("./src/commands");
+    for (const folder of commandFolders) {
+      const commandFiles = fs
+        .readdirSync(`./src/commands/${folder}`)
+        .filter((file) => file.endsWith(".js"));
+
+      const { commands, commandArray } = client;
+      for (const file of commandFiles) {
+        const command = require(`../../commands/${folder}/${file}`);
+        commands.set(command.data.name, command);
+        commandArray.push(command.data.toJSON());
+        console.log(
+          `Command: ${command.data.name} has successfully passed through handler.`
+        );
+      }
+    }
+
+    const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
+
+    try {
+      console.log(
+        `Started refreshing ${client.commandArray.length} application (/) commands.`
+      );
+
+      // The put method is used to fully refresh all commands in the guild with the current set
+      const data = await rest.put(
+        Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+        { body: client.commandArray }
+      );
+
+      console.log(
+        `Successfully reloaded ${data.length} application (/) commands.`
+      );
+    } catch (error) {
+      // And of course, make sure you catch and log any errors!
+      console.error(error);
+    }
+  };
+};
