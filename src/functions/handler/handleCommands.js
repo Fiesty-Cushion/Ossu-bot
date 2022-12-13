b83eb60db6da57cda5b40d984f46e5c2610bb3f5
Fiesty-Cushion <neupane.braz07@gmail.com>
2022-12-13 23:56:16 +0545

    multi-guild functional

--- src/functions/handler/handleCommands.js
+++ src/functions/handler/handleCommands.js
@@ -30,7 +30,7 @@ module.exports = (client) => {
 
       // The put method is used to fully refresh all commands in the guild with the current set
       const data = await rest.put(
-        Routes.applicationGuildCommands(process.env.CLIENT_ID , process.env.GUILD_ID),
+        Routes.applicationCommands(process.env.CLIENT_ID /*, process.env.GUILD_ID*/),
         { body: client.commandArray }
       );
 
